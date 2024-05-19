6fdf3674e133175ed2e2149f1808d5cf06dd55a8
Gustav Brodin <brodin.gustav@gmail.com>
2024-05-19 14:29:12 +0200

    Commit 3 - Protected.js - Lagt in funktion för att hämta skyddad data samt funktion för att logga ut.
    protected.html - Lagt enkel layout för html, samt länkar till script vid laddning av sida för rendering av protected.html
    
    Måste fundera hur göra med rendering av dynamisk meny?

---
+++ src/protected.js
@@ -0,0 +1,37 @@
+//Definierar URL:s
+const Url = "http://localhost:3000/api";
+const url_Login = `${Url}/login`;
+const url_Protected = `${Url}/Protected`;
+
+//Funktion för att hämta skyddad data
+async function getProtectedData() {
+    try{
+        const token = localStorage.getItem("token");
+        const response = await fetch(url_Protected, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        const data = await response.json();
+        console.log(data);
+
+        //Hämtar ID
+        const protectedContent = document.getElementById("protectedContent");
+        protectedContent.classList.add("protectedContent")
+        protectedContent.innerHTML = `<p>Välkommen anställd nr 666</p>`
+        const protectedData = document.getElementById("protectedData")
+        protectedData.classList.add("protectedData")
+    
+    } catch (error) {
+        console.error("Error vid fetching av data:", error);
+    }
+}
+
+//Funktion för att logga ut
+function logOut() {
+    localStorage.removeItem("token");                                           //Tar bort token vid utlogg
+    document.getElementById("protectedData").style.display = "none";            //Gömmer ID vid utlogg
+    window.location.href = 'index.html';
+}
